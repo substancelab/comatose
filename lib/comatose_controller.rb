e7f17678c318c029b949324b62258c51dc1fdf4f
Nic Martin <niciliketo@gmail.com>
2013-04-19 14:22:44 +0100

    re-add support for comatose_root other than ''

--- lib/comatose_controller.rb
+++ lib/comatose_controller.rb
@@ -1,14 +1,14 @@
 # The controller for serving cms content...
-class ComatoseController < ActionController::Base 
+class ComatoseController < ActionController::Base
 	include ExceptionLogger::ExceptionLoggable # loades the module
   rescue_from Exception, :with => :log_exception_handler # tells rails to forward the 'Exception' (you can change the type) to the handler of the module
 
   unloadable
-  
+
   before_filter :handle_authorization, :set_content_type
   after_filter :cache_cms_page
   helper :application
-    
+
   # Render a specific page
   def show
     page_name, page_ext = get_page_path
@@ -23,7 +23,7 @@ class ComatoseController < ActionController::Base
       render :nothing=>true, :status=>status
       #raise ActiveRecord::RecordNotFound.new("Comatose page not found ")
     else
-      # Make the page access 'safe' 
+      # Make the page access 'safe'
       @page = Comatose::PageWrapper.new(page)
       # For accurate uri creation, tell the page class which is the active mount point...
       ComatosePage.active_mount_info = get_active_mount_point(params[:index])
@@ -83,6 +83,11 @@ protected
     elsif !params[:index].empty?
       page_name = "#{params[:index]}/#{page_name}"
     end
+    if page_name != ""
+      page_name = "/" << page_name
+    end
+    page_name =  params[:root] << page_name
+
     return page_name, page_ext
   end
 
@@ -92,7 +97,7 @@ protected
     params[:layout]
   end
 
-  # An after_filter implementing page caching if it's enabled, globally, 
+  # An after_filter implementing page caching if it's enabled, globally,
   # and is allowed by #allow_page_cache?
   def cache_cms_page
     unless Comatose.config.disable_caching or response.headers['Status'] == '404 Not Found'
@@ -127,7 +132,7 @@ protected
     end
     include mod_klass
   end
-  
+
   # Include any helpers...
   Comatose.config.helpers.each do |mod|
     mod_klass = if mod.is_a? String
